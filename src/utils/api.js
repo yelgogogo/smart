94b7a3d80685ecd1755db9f3132b3b7352078314
yelgogogo <yelgogogo@hotmail.com>
2018-06-24 21:05:22 +0800

    更新 prod api地址

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const isProduction = !!(process.env.NODE_ENV === 'production')
-// const baseURL = isProduction ? 'http://58.48.199.131:7777' : 'http://58.48.199.131:7777'
-const baseURL = isProduction ? 'http://50.116.4.46:8082' : 'http://50.116.4.46:8082'
+// const baseURL = isProduction ? 'http://120.24.244.225/amazonweb' : 'http://50.116.4.46:8082'
+const baseURL = isProduction ? 'http://120.24.244.225/amazonweb' : 'http://50.116.4.46:8082'
 const tokenKey = 'x-auth-token'
 
 // smart service
