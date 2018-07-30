bc44d62ff4b57f4d91d4569531315f39207dd9ce
Susie-Yang <19610645@qq.com>
2018-07-30 16:47:29 +0800

    update field name

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -194,7 +194,7 @@ export default {
       //     }
       //   }
       // }
-      api.post('/api/suggestion/count', {fiilter: {}}).then(res => {
+      api.post('/api/suggestion/count', {filter: {}}).then(res => {
         if (res.status === 200 && res.data) {
           // this.workflows = res.data.grid
           console.log(this.workflow)
